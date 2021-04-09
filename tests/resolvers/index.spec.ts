8183d2f17e1f99f64aacebb1c980a42ec2f5b662
Christian Farris <christianfarriscs@gmail.com>
2021-04-09 00:10:51 -0500

    test resolver map

---
+++ tests/resolvers/index.spec.ts
@@ -0,0 +1,16 @@
+import { resolvers } from '../../src/resolvers/index';
+import * as HelloResolvers from '../../src/resolvers/hello-resolvers';
+
+jest.mock('../../src/resolvers/hello-resolvers');
+
+const { helloResolver } = HelloResolvers as jest.Mocked<typeof HelloResolvers>;
+
+describe('Resolver Map', () => {
+  test('should have expected fields', () => {
+    expect(resolvers).toStrictEqual({
+      Query: {
+        hello: helloResolver,
+      },
+    });
+  });
+});
