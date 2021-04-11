75dbdfbe3eeb50f807587c6bc428c41f7ff6aa1e
Christian Farris <christianfarriscs@gmail.com>
2021-04-10 19:53:14 -0500

    change to port 8080

--- acceptance/graphql/hello.spec.ts
+++ acceptance/graphql/hello.spec.ts
@@ -3,7 +3,7 @@ import { request, gql } from 'graphql-request';
 describe('When the Hello query is called', () => {
   test('Then it should return Hello World', async () => {
     const data = await request(
-      'http://localhost:4000/graphql',
+      'http://localhost:8080/graphql',
       gql`
         query Hello {
           hello
