4f96eb4f15ef29b9b3c82fcc3f721839ae163eaa
Christian Farris <christianfarriscs@gmail.com>
2021-04-13 00:37:30 -0500

    make tests fail

--- tests/resolvers/hello-resolvers.spec.ts
+++ tests/resolvers/hello-resolvers.spec.ts
@@ -3,7 +3,7 @@ import { helloResolver } from '../../src/resolvers/hello-resolvers';
 describe('hello-resolvers', () => {
   describe('helloResolver', () => {
     test('should say hello world', () => {
-      expect(helloResolver()).toStrictEqual('Hello World!');
+      expect(helloResolver()).toStrictEqual('Hello World');
     });
   });
 });
