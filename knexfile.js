ecf5c7b5229a480fadf95c29997885e85bd2601f
Christian Farris <christianfarriscs@gmail.com>
2021-04-13 00:31:59 -0500

    fix lint errors

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,9 @@ const config = {
     connection: {
       database: process.env.DB_NAME,
       password: process.env.DB_PASSWORD,
-      user:     process.env.DB_USER,
-    }
+      user: process.env.DB_USER,
+    },
   },
 };
 
-export default config
+export default config;
