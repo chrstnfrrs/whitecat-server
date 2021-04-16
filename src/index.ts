ed26bb202ea9a07b5ccd5d8adb716fe0a0cfdc00
Christian Farris <christianfarriscs@gmail.com>
2021-04-15 19:10:07 -0500

    remove migrations

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,6 @@ const server = new ApolloServer({
   schema,
 });
 
-const runMigrations = async () => {
-  try {
-    const hasUsers = await db.schema.hasTable('users');
-    console.log('hasUsers', hasUsers);
-  } catch (error) {
-    console.log('run migrations error', error);
-  }
-};
-
-try {
-  runMigrations();
-} catch (error) {
-  console.log('error', error);
-}
-
 server.listen(process.env.PORT || 8080).then(() => {
   console.log('🚀  Server ready at http://localhost:8080/graphql');
 });
