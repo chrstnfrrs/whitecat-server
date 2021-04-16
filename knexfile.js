c5d49ee1176fc8c71e7584cb123278ad608537fa
Christian Farris <christianfarriscs@gmail.com>
2021-04-15 19:01:49 -0500

    try knexfile

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,14 @@ const config = {
     client: 'pg',
     connection: {
       database: process.env.DB_NAME,
+      // host: process.env.DB_HOST,
+      host: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
       password: process.env.DB_PASSWORD,
       user: process.env.DB_USER,
+    },
+    pool: {
+      max: 20,
+      min: 1,
     },
   },
 };
