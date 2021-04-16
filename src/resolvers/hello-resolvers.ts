9e0761965a2e002d14d13f7830dec929f4e4f3dd
Christian Farris <christianfarriscs@gmail.com>
2021-04-15 19:10:53 -0500

    try db query in resolver

--- src/resolvers/hello-resolvers.ts
+++ src/resolvers/hello-resolvers.ts
@@ -1,3 +1,9 @@
-const helloResolver = () => 'Hello World';
+import db from 'querybuilder';
+
+const helloResolver = async (): Promise<string> => {
+  const hasUsers = await db.schema.hasTable('users');
+
+  return hasUsers.toString();
+};
 
 export { helloResolver };
