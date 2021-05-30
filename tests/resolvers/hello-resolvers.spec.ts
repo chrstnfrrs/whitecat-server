5923581206ee346a3048dab35e957da41ac0fbf1
Christian Farris <christianfarriscs@gmail.com>
2021-05-30 03:10:29 -0500

    remove hello resolvers

--- tests/resolvers/hello-resolvers.spec.ts
+++
@@ -1,9 +0,0 @@
-import { helloResolver } from '../../src/resolvers/hello-resolvers';
-
-describe('hello-resolvers', () => {
-  describe('helloResolver', () => {
-    test('should say hello world', () => {
-      expect(helloResolver()).toStrictEqual('Hello World');
-    });
-  });
-});
