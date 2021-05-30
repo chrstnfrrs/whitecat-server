56e9a4ee42a38846d431da62e2947aa434c21a1d
Christian Farris <christianfarriscs@gmail.com>
2021-05-30 03:10:55 -0500

    test hello schema

---
+++ tests/schema/hello-schema.spec.ts
@@ -0,0 +1,13 @@
+import { gql } from 'apollo-server';
+
+import HelloSchema from '../../src/schema/hello-schema';
+
+describe('Given the HelloSchema', () => {
+  test('should be expected type', () => {
+    expect(HelloSchema).toStrictEqual(gql`
+      extend type Query {
+        hello: String!
+      }
+    `);
+  });
+});
