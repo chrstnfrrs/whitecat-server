eaa578b228330d7f50f475297e7d5505015bcbda
Christian Farris <christianfarriscs@gmail.com>
2021-05-30 03:10:37 -0500

    test resolver map

--- tests/resolvers/index.spec.ts
+++ tests/resolvers/index.spec.ts
@@ -1,16 +1,34 @@
 import { resolvers } from '../../src/resolvers';
-import * as HelloResolvers from '../../src/resolvers/hello-resolvers';
+import * as UserResolvers from '../../src/resolvers/user-resolvers';
 
-jest.mock('../../src/resolvers/hello-resolvers');
+jest.mock('../../src/resolvers/user-resolvers');
 
-const { helloResolver } = HelloResolvers as jest.Mocked<typeof HelloResolvers>;
+const {
+  create: createUser,
+  del: deleteUser,
+  update: updateUser,
+  getById: user,
+  getWhere: users,
+} = UserResolvers as jest.Mocked<typeof UserResolvers>;
 
 describe('Resolver Map', () => {
   test('should have expected fields', () => {
-    expect(resolvers).toStrictEqual({
-      Query: {
-        hello: helloResolver,
-      },
-    });
+    expect(JSON.stringify(resolvers)).toStrictEqual(
+      JSON.stringify({
+        Mutation: {
+          createUser,
+          deleteUser,
+          updateUser,
+        },
+        Query: {
+          hello: (): string => 'Hello',
+          user,
+          users,
+        },
+      }),
+    );
+  });
+  test('hello should return hello', () => {
+    expect(resolvers.Query.hello()).toStrictEqual('Hello');
   });
 });
