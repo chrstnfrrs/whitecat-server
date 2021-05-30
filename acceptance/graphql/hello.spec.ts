f243ab2e8e702ee6f4c453326fd6328385984b74
Christian Farris <christianfarriscs@gmail.com>
2021-05-30 03:33:06 -0500

    log why fails

--- acceptance/graphql/hello.spec.ts
+++ acceptance/graphql/hello.spec.ts
@@ -2,15 +2,20 @@ import { request, gql } from 'graphql-request';
 
 describe('When the Hello query is called', () => {
   test('Then it should return Hello', async () => {
-    const data = await request(
-      `http://localhost:${process.env.PORT || 8080}/graphql`,
-      gql`
-        query Hello {
-          hello
-        }
-      `,
-    );
+    try {
+      const data = await request(
+        `http://localhost:${process.env.PORT || 8080}/graphql`,
+        gql`
+          query Hello {
+            hello
+          }
+        `,
+      );
 
-    expect(data.hello).toStrictEqual('Hello');
+      expect(data.hello).toStrictEqual('Hello');
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log('error', error);
+    }
   });
 });
