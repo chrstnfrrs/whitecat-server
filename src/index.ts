f26e02157fe1669eda5aa6d48e366d284d93f046
Christian Farris <christianfarriscs@gmail.com>
2021-05-29 22:34:33 -0500

    remove migrations from server start

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import { ApolloServer, makeExecutableSchema } from 'apollo-server';
 import 'dotenv/config';
 
-import { migrations } from './migrations';
 import { typeDefs } from './types';
 import { resolvers } from './resolvers';
 
-const startServer = async () => {
-  await migrations();
-
+const startServer = () => {
   const schema = makeExecutableSchema({
     resolvers,
     typeDefs,
@@ -20,6 +17,7 @@ const startServer = async () => {
   });
 
   server.listen(process.env.PORT || 8080).then(() => {
+    // eslint-disable-next-line no-console
     console.log('🚀  Server ready at http://localhost:8080/graphql');
   });
 };
