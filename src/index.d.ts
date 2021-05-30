6db51be6ec0fbda049da3007f1566e067aa8be16
Christian Farris <christianfarriscs@gmail.com>
2021-05-30 03:03:20 -0500

    Add User types , GraphqlError, and AllowAny

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,13 @@
+import * as User from './types/user-types.d';
+
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IContext {}
+interface GraphqlError {
+  error: Error;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AllowAny = any;
 type Uuid = string;
 
-export { IContext, Uuid };
+export { AllowAny, IContext, GraphqlError, Uuid, User };
