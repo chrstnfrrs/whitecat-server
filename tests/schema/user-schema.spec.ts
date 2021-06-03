a3adeef9de25fda75c190471e0811f6c53d59595
Christian Farris <christianfarriscs@gmail.com>
2021-06-02 21:08:47 -0500

    test user schema

---
+++ tests/schema/user-schema.spec.ts
@@ -0,0 +1,46 @@
+import { gql } from 'apollo-server';
+
+import UserSchema from '../../src/schema/user-schema';
+
+describe('Given the UserSchema', () => {
+  test('Then should be expected type', () => {
+    expect(UserSchema).toStrictEqual(gql`
+      extend type Query {
+        user(id: ID!): User
+        users(where: UserWhere): [User]!
+      }
+
+      extend type Mutation {
+        createUser(input: UserInput!): User!
+        updateUser(id: ID!, input: UserUpdate!): User!
+        deleteUser(id: ID!): Boolean!
+      }
+
+      type User {
+        id: ID!
+        email: String!
+        firstName: String!
+        lastName: String!
+        weights: [Weight!]!
+      }
+
+      input UserInput {
+        email: String!
+        firstName: String!
+        lastName: String!
+      }
+
+      input UserUpdate {
+        email: String
+        firstName: String
+        lastName: String
+      }
+
+      input UserWhere {
+        email: String
+        firstName: String
+        lastName: String
+      }
+    `);
+  });
+});
