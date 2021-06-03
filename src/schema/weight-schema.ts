25a86cbcaa09cf78383035108f15d19335e716ec
Christian Farris <christianfarriscs@gmail.com>
2021-06-02 20:33:35 -0500

    create weight typedefs

---
+++ src/schema/weight-schema.ts
@@ -0,0 +1,41 @@
+import { gql } from 'apollo-server';
+
+const typeDefs = gql`
+  extend type Query {
+    weights(id: ID!) [Weight!]!
+    weights(where: WeightWhere!) [Weight!]!
+  }
+
+  extend type Mutation {
+    createWeight(input: WeightInput!): Weight!
+    updateWeight(id: ID!, input: WeightUpdate!): User!
+    deleteWeight(id: ID!): Boolean!
+  }
+
+  type Weight {
+    id: ID!
+    weight: Float!
+    date: String!
+    user: User!
+  }
+
+  input WeightInput {
+    userId: ID!
+    weight: Float!
+    date: String
+  }
+
+  input WeightUpdate {
+    weight: Float
+    date: String
+  }
+
+  input WeightWhere {
+    id: ID
+    userId: ID
+    weight: Float
+    date: String
+  }
+`;
+
+export default typeDefs;
