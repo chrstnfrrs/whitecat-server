b832b529347494d594ad3e93c213e363d50db90e
Christian Farris <christianfarriscs@gmail.com>
2021-06-06 23:21:36 -0500

    create AT setup constants for user type

---
+++ tests/acceptance/constants/user.ts
@@ -0,0 +1,10 @@
+const USER_UUID = '21be509d-162b-48f3-9a39-b7b28f66789e';
+const USER = {
+  email: 'admin@test.com',
+  firstName: 'Christian',
+  id: USER_UUID,
+  lastName: 'Farris',
+};
+const USERS = [USER];
+
+export { USER, USERS, USER_UUID };
