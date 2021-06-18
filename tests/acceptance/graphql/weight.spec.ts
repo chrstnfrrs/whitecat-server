9df110406a9c936878fa3f08e54b66e5016763c5
Christian Farris <christianfarriscs@gmail.com>
2021-06-18 01:25:21 -0500

    create createWeight AT

--- tests/acceptance/graphql/weight.spec.ts
+++ tests/acceptance/graphql/weight.spec.ts
@@ -1,4 +1,56 @@
-describe('Given the User Model', () => {
+import { request, gql } from 'graphql-request';
+
+import * as Types from '../../../src/index.d';
+import * as WeightConstants from '../constants/weight';
+import * as WeightFactory from '../../model-factories/weight-model-factories';
+
+describe('Given the Weight Model', () => {
+  let currentWeight: Types.AllowAny;
+
+  describe('When creating weight', () => {
+    let newWeight: Types.Weight.Input;
+
+    beforeEach(async () => {
+      newWeight = WeightFactory.createRandomWeightInput({
+        userId: WeightConstants.WEIGHT_USER_UUID,
+      });
+
+      const data: Types.AllowAny = await request(
+        `http://localhost:${process.env.PORT || 8080}`,
+        gql`
+          mutation CreateWeight($input: WeightInput!) {
+            createWeight(input: $input) {
+              id
+              date
+              weight
+              userId
+            }
+          }
+        `,
+        {
+          input: newWeight,
+        },
+      );
+
+      currentWeight = data.createWeight;
+    });
+
+    afterEach(async () => {
+      await request(
+        `http://localhost:${process.env.PORT || 8080}`,
+        gql`
+          mutation DeleteWeight($id: ID!) {
+            deleteWeight(id: $id)
+          }
+        `,
+        { id: currentWeight.id },
+      );
+    });
+
+    test('Then create the expected weight', () => {
+      expect(currentWeight).toMatchObject(newWeight);
+    });
+  });
   // describe('When creating a weight', () => {});
   // describe('When querying a specific weight', () => {});
   // describe('When querying for weights', () => {});
