465c2b9be342ec321594dc94cc2c8afa7af18cc2
Christian Farris <christianfarriscs@gmail.com>
2021-06-18 01:06:05 -0500

    ADD WEIGHT USER

--- tests/acceptance/constants/weight.ts
+++ tests/acceptance/constants/weight.ts
@@ -1,5 +1,13 @@
 import { USER_UUID } from './user';
 
+const WEIGHT_USER_UUID = '539810cd-5965-4512-88bd-daf2cc505c1a';
+const WEIGHT_USER = {
+  email: 'weight_user@whitecat.com',
+  firstName: 'Christian',
+  id: WEIGHT_USER_UUID,
+  lastName: 'Farris',
+};
+const WEIGHT_USERS = [WEIGHT_USER];
 const WEIGHT_UUID = 'cab7c4af-3702-4b63-8254-1ba2863a01d5';
 const WEIGHT = {
   date: '2099-06-12T11:50:28.711Z',
@@ -9,4 +17,11 @@ const WEIGHT = {
 };
 const WEIGHTS = [WEIGHT];
 
-export { WEIGHT, WEIGHTS, WEIGHT_UUID };
+export {
+  WEIGHT,
+  WEIGHTS,
+  WEIGHT_UUID,
+  WEIGHT_USER_UUID,
+  WEIGHT_USER,
+  WEIGHT_USERS,
+};
