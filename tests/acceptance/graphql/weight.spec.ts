89a6b497345f3a8325532decb9adf66b67e7dc94
Christian Farris <christianfarriscs@gmail.com>
2021-06-17 23:58:27 -0500

    begin work on WEIGHT ATs

---
+++ tests/acceptance/graphql/weight.spec.ts
@@ -0,0 +1,10 @@
+describe('Given the User Model', () => {
+  // describe('When creating a weight', () => {});
+  // describe('When querying a specific weight', () => {});
+  // describe('When querying for weights', () => {});
+  // describe('When updating a weight', () => {});
+  // describe('When deleting a weight', () => {});
+  test('should be true', () => {
+    expect(true).toStrictEqual(true);
+  });
+});
