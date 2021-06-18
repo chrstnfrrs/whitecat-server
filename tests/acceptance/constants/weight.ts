e0dad17d40f0a0d14ac1e2906a62f7efe17f75ec
Christian Farris <christianfarriscs@gmail.com>
2021-06-17 23:58:08 -0500

    create WEIGHT AT constants

---
+++ tests/acceptance/constants/weight.ts
@@ -0,0 +1,12 @@
+import { USER_UUID } from './user';
+
+const WEIGHT_UUID = 'cab7c4af-3702-4b63-8254-1ba2863a01d5';
+const WEIGHT = {
+  date: '2099-06-12T11:50:28.711Z',
+  id: WEIGHT_UUID,
+  userId: USER_UUID,
+  weight: 214.28,
+};
+const WEIGHTS = [WEIGHT];
+
+export { WEIGHT, WEIGHTS, WEIGHT_UUID };
