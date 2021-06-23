81728ad893e9102905de08071f247b451098312d
Christian Farris <christianfarriscs@gmail.com>
2021-06-22 22:47:11 -0500

    adjust weight constants

--- tests/acceptance/constants/weight.ts
+++ tests/acceptance/constants/weight.ts
@@ -8,16 +8,26 @@ const WEIGHT_USER = {
   lastName: 'Farris',
 };
 const WEIGHT_USERS = [WEIGHT_USER];
-const WEIGHT_UUID = 'cab7c4af-3702-4b63-8254-1ba2863a01d5';
-const WEIGHT = {
+const DELETE_WEIGHT_UUID = 'cab7c4af-3702-4b63-8254-1ba2863a01d5';
+const DELETE_WEIGHT = {
   date: '2099-06-12T11:50:28.711Z',
-  id: WEIGHT_UUID,
+  id: DELETE_WEIGHT_UUID,
   userId: USER_UUID,
   weight: 214.28,
 };
-const WEIGHTS = [WEIGHT];
+const WEIGHT_UUID = 'ed828391-0c34-4766-b853-8e40cf584990';
+const WEIGHT = {
+  date: '2011-11-11T11:11:11.711Z',
+  id: WEIGHT_UUID,
+  userId: USER_UUID,
+  weight: 193.53,
+};
+const WEIGHTS = [WEIGHT, DELETE_WEIGHT];
 
 export {
+  USER_UUID,
+  DELETE_WEIGHT_UUID,
+  DELETE_WEIGHT,
   WEIGHT,
   WEIGHTS,
   WEIGHT_UUID,
