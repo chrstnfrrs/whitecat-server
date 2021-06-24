c52edbad8c3d6ab8494a469fce75176c9ab1028d
Christian Farris <christianfarriscs@gmail.com>
2021-06-23 23:48:53 -0500

    tad

--- tests/src/schema/weight-schema.spec.ts
+++ tests/src/schema/weight-schema.spec.ts
@@ -6,39 +6,40 @@ describe('Given the WeightSchema', () => {
   test('Then should be expected type', () => {
     expect(WeightSchema).toStrictEqual(gql`
       extend type Query {
-        weight(id: ID!): [Weight!]!
+        weight(id: ID!): Weight
         weights(where: WeightWhere!): [Weight!]!
       }
 
       extend type Mutation {
         createWeight(input: WeightInput!): Weight!
-        updateWeight(id: ID!, input: WeightUpdate!): User!
+        updateWeight(id: ID!, input: WeightUpdate!): Weight!
         deleteWeight(id: ID!): Boolean!
       }
 
       type Weight {
         id: ID!
         weight: Float!
-        date: String!
+        date: Date!
         user: User!
+        userId: ID!
       }
 
       input WeightInput {
         userId: ID!
         weight: Float!
-        date: String
+        date: Date
       }
 
       input WeightUpdate {
         weight: Float
-        date: String
+        date: Date
       }
 
       input WeightWhere {
         userId: ID!
         id: ID
         weight: Float
-        date: String
+        date: Date
       }
     `);
   });
